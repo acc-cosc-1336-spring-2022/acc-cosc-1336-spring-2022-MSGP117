9bb44486226409db4eb2b4171a211a1228454d59
MSGP117 <mason.garcia-paredes@g.austincc.edu>
2022-02-17 21:25:32 -0600

    Homework 2 Complete

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,14 @@
-#
+def get_options_ratio(options, total):
+    return options / total
+
+def get_faculty_rating(get_options_ratio):
+    if get_options_ratio > .9 and get_options_ratio < 1:
+        return "Excellent"
+    if get_options_ratio > .8 and get_options_ratio < .9:
+        return "Very Good"
+    if get_options_ratio > .7 and get_options_ratio < .8:
+        return "Good"
+    if get_options_ratio > .6 and get_options_ratio < .7:
+        return "Needs Improvement"
+    if get_options_ratio > 0 and get_options_ratio < .6:
+        return "Unacceptable"
