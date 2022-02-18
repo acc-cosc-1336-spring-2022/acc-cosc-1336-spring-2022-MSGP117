65010081f788de119a0b0ab7333f7e021b2a9986
MSGP117 <mason.garcia-paredes@g.austincc.edu>
2022-02-17 21:27:04 -0600

    Homework 2 Complete

--- src/examples/c_decisions/decisions.py
+++ src/examples/c_decisions/decisions.py
@@ -1,2 +1,32 @@
 def test_config():
     return True
+
+def get_letter_grade(grade):
+    if grade >= 90 and grade <= 100:
+        return 'A'
+    elif grade >= 80 and grade < 90:
+        return 'B'
+    elif grade >= 70 and grade < 80:
+        return 'C'
+    elif grade >= 60 and grade < 70:
+        return 'D'
+    elif grade > 0 and grade <= 59:
+        return 'F'
+    elif grade < 0 and grade > 100:
+        return 'Invalid Number'
+
+def logical_op_precedence(op1, op2, op3):
+    return not op1 and op2 or op3
+
+def number_is_in_range_and(min, max, num):
+    return num >= min and num <= max
+
+def number_is_not_in_range(min, max, num):
+    return not (num <= min and num >= max)
+
+def num_is_not_in_range_or(min, max, num):
+    return num < min or num > max
+
+def is_letter_consonant(letter):
+    return not (letter == 'a' or letter == 'e' or letter == 'o' or letter == 'u' or letter == '1')
+
--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,2 +1,9 @@
 import repetition
 
+#repetition.display_numbers(3)
+
+#repetition.for_intro_loop_strings()
+#repetition.for_num_in_range(5)
+#repetition.for_num_in_range_w_start_value(1, 5)
+#repetition.for_num_range_w_step_value(0, 10, 2)
+repetition.for_display_sum_of_squares(1, 11)
