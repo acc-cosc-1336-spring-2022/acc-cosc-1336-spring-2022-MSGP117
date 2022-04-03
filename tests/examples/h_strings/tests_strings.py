46fdfa5da8f8eb887d2c79fe205b8a0064d6903d
MSGP117 <mason.garcia-paredes@g.austincc.edu>
2022-04-02 23:41:16 -0500

    Homework 5

--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -1,9 +1,11 @@
 import unittest
 
-from src.examples.h_strings.strings import test_config
+from src.examples.h_strings.strings import concat_strings, test_config
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_concat_strings(self):
+        self.assertEqual("john doe", concat_strings("john ", "doe"))
